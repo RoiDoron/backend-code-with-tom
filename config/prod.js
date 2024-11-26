4abf767e43092e31cccf112015676a695b9d8c7a
Roi Doron <roidoron6@gmail.com>
2024-11-26 22:49:22 +0200

    Forget password

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://roiDoron:<db_password>@masterjs.jub11.mongodb.net/?retryWrites=true&w=majority&appName=masterJS',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://roiDoron:1d2o3r4o5n@masterjs.jub11.mongodb.net/?retryWrites=true&w=majority&appName=masterJS',
   dbName : process.env.DB_NAME || 'code_db'
 }
